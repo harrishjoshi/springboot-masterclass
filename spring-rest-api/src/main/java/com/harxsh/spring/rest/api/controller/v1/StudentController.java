643a526fc47927a3d55f7fb0b0b13220c9e54891
harxshjoshi <dev.harishjoshi@gmail.com>
2022-09-04 17:57:49 +0545

    slf4j-api, spring profiles, and properties file add

--- spring-rest-api/src/main/java/com/harxsh/spring/rest/api/controller/v1/StudentController.java
+++ spring-rest-api/src/main/java/com/harxsh/spring/rest/api/controller/v1/StudentController.java
@@ -1,16 +1,32 @@
 package com.harxsh.spring.rest.api.controller.v1;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController("v1.StudentController")
 @RequestMapping("/api/v1/students")
+@PropertySource("classpath:test.properties")
+@Slf4j
 public class StudentController {
+
+    @Value("${test.message}")
+    private String testMessage;
+
+    @GetMapping("test")
+    @ResponseBody
+    public String test() {
+        log.info("Inside test method: {}", testMessage);
+        return testMessage;
+    }
 
     @GetMapping("")
     public ResponseEntity<List<Student>> getStudents() {
