df4574cd1ef64eefacebb326e55f4b5dcb75cf94
harxshjoshi <dev.harishjoshi@gmail.com>
2022-09-05 21:16:25 +0545

    Custom endpoint using Spring Actuator

---
+++ spring-rest-api/src/main/java/com/harxsh/spring/rest/api/config/FeatureEndpoint.java
@@ -0,0 +1,42 @@
+package com.harxsh.spring.rest.api.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Endpoint(id = "features")
+public class FeatureEndpoint {
+
+    private final Map<String, Feature> featureMap = new ConcurrentHashMap<>();
+
+    public FeatureEndpoint() {
+        featureMap.put("Student", new Feature(true));
+        featureMap.put("Address", new Feature(false));
+        featureMap.put("Authentication", new Feature(false));
+    }
+
+    @ReadOperation
+    public Map<String, Feature> features() {
+        return featureMap;
+    }
+
+    @ReadOperation
+    public Feature feature(@Selector String featureName) {
+        return featureMap.get(featureName);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    private static class Feature {
+        private boolean isEnabled;
+    }
+}
